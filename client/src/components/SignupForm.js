45e064bbded085ffc744d436de86c7ba3024781b
Lucas DiFalco <luke.difalco52@gmail.com>
2021-10-30 15:45:04 -0400

    debugged sign up form, removed unneeded functions, cleaned up code to read easier.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -9,15 +9,22 @@ import {ADD_USER} from '../utils/mutations.js';
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState(
+    { username: '', 
+    email: '', 
+    password: '' });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const [addUser, {error, data}] =  useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({ 
+      ...userFormData, 
+      [name]: value });
   };
 
   const handleFormSubmit = async (event) => {
@@ -31,15 +38,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+        const { data } = await addUser({
+            variables: {...userFormData}
+        });
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+        Auth.login(data.addUser.token);
+      
+
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
