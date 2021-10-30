6d9ad34713b6115c65c2acc1147b5e0cfd4d5199
Lucas DiFalco <luke.difalco52@gmail.com>
2021-10-30 18:31:21 -0400

    debugged server.js.....why wont the server work?

--- server/server.js
+++ server/server.js
@@ -1,21 +1,41 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const {authMiddleware} = require('./utils/auth.js');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+async function runApolloServer() {
+  const PORT = process.env.PORT || 3001;
+  const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 }
 
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+runApolloServer();
