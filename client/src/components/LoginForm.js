4a3ef0c4b785d7ae9dc176b1ec1b64d531156350
Lucas DiFalco <luke.difalco52@gmail.com>
2021-10-29 21:03:39 -0400

    debugged loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,10 +5,15 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import {gql, useMutation} from '@apollo/client';
+import {LOGIN_USER} from '../utils/mutation.js';
+
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser, {error, data}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,17 +31,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await loginUser({
+        variables: {...userFormData}
+      });
+      Auth.login(data.login.token);
+      
     } catch (err) {
-      console.error(err);
+      console.err(err);
       setShowAlert(true);
     }
 
