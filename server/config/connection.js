05bfd5dcb10135d182ee834b86542e0c0af2fb5a
Lucas DiFalco <luke.difalco52@gmail.com>
2021-11-03 22:34:26 -0400

    pls work :(

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Search', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
