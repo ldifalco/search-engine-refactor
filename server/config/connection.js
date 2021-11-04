4388d17e69cab6269ab6f1a61d0bcb4da25f04d3
Lucas DiFalco <luke.difalco52@gmail.com>
2021-11-03 22:28:36 -0400

    Deploying.....again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
